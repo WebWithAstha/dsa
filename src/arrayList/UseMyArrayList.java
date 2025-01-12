308d038621e2fe6b1bd93ceb167b74cd799c1855
WebWithAstha <lodhiastha85@gmail.com>
2025-01-12 21:51:51 +0530

    myArrayList

---
+++ src/arrayList/UseMyArrayList.java
@@ -0,0 +1,9 @@
+package arrayList;
+
+public class UseMyArrayList {
+
+    public static void main(String[] args) {
+        MyArrayList arr = new MyArrayList();
+        System.out.println(arr);
+    }
+}
