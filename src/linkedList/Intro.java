a53f809ef9d37990bc13405c52cf6742fb0c24f8
WebWithAstha <lodhiastha85@gmail.com>
2025-02-10 23:32:44 +0530

    linkedlist intro

---
+++ src/linkedList/Intro.java
@@ -0,0 +1,19 @@
+package linkedList;
+
+//import java.util.LinkedList;
+
+public class Intro {
+    public static void main(String[] args) throws Exception {
+       MyLinkedList ml = new MyLinkedList(0);
+//        ml.add(1);
+//        ml.add(2);
+//        ml.add(3);
+//        System.out.println(ml.size());
+//        ml.add(3,66);
+//        ml.addFirst(99);
+        System.out.println(ml.removeFirst());
+        System.out.println(ml.removeFirst());
+        System.out.println(ml);
+
+    }
+}
