945d1b345239ea1a2fa29def6a0067983481a13e
WebWithAstha <lodhiastha85@gmail.com>
2025-02-11 21:16:31 +0530

    stack next large

---
+++ src/stack/NextLarge.java
@@ -0,0 +1,17 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NextLarge {
+    public static void main(String[] args) {
+        ArrayList<Integer> ans  = new ArrayList<>();
+        ans.add(2);
+        ans.add(3);
+        Stack<Integer> st =  new Stack<>();
+st.push(7);
+            st.clear();
+        System.out.println(st);
+
+    }
+}
