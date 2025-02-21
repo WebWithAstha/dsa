485960cef5e5a848e8969a030167b6e07e1662f4
WebWithAstha <lodhiastha85@gmail.com>
2025-02-21 21:13:28 +0530

    reverse of k group

---
+++ src/recursion/ReverseKGroup.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class ReverseKGroup {
+    public static void main(String[] args) {
+
+        // declare start and end
+        // determine ki valid end h ki nhi
+        // agr nhi to return start
+        // agr h to age bdo
+
+        // aage ka reverse mago jo head return krege
+
+        // khud ka segment reverse kro
+        // returned head ko apne end k next me add kro
+        // end return kro
+
+
+
+    }
+}
